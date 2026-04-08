07df120052e1d74dd7cbbcc9e069faf46184d77e
unknown <cesar@lumengroup.com.co>
2026-04-08 13:35:52 -0500

    variables de ingreso

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Reemplaza los textos entre comillas con tus llaves de Supabase
-const supabaseUrl = 'https://zqjzuehcqkrzkticjfuh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxanp1ZWhjcWtyemt0aWNqZnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDQ2MjIsImV4cCI6MjA4ODkyMDYyMn0.cj6tbF2mR9whaaJ8EFYYb-rsq3Ka0RLQjt4qAay80rQ';
+// Ahora el código le pedirá las llaves de forma segura a Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
